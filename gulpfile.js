125a337839048e0c08c3686829737b585e491d32
CanonLs <467706369@qq.com>
2017-09-25 16:34:03 +0800

    devGlup

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,29 @@
-var gulp        = require('gulp');
-uglify          = require('gulp-uglify');
-cleanCSS        = require('gulp-clean-css');
-autoprefixer    = require('gulp-autoprefixer');
-bs              = require('browser-sync');
-rename          = require('gulp-rename');
-zip             = require('gulp-zip');
-sass 		    = require('gulp-sass'),
-concat 		    = require('gulp-concat'),
-htmlmin 	    = require('gulp-htmlmin'),
-plumber         = require('gulp-plumber');
-del             = require('del');
-dn              = process.platform == "win32" ? __dirname.toString().split('\\').pop() : __dirname.toString().split('/').pop(),
-path = {
-    css: 	'develop/css/style/*',
-    sass:	'develop/css/style/*.css',
-    js: 	'develop/js/*.js',
-    images: 'develop/images/**/*.{png,jpg,gif,ico}',
-    html: 	'develop/*.html',
-    all:    'develop/**/*',
-    devCss:	'develop/css/*.css',
-    outCss: 'develop/css'
-},
+var gulp            = require('gulp');
+    autoprefixer    = require('gulp-autoprefixer'),
+    bs              = require('browser-sync'),
+    sass 		    = require('gulp-sass'),
+    plumber         = require('gulp-plumber'),
+    path = {
+        css: 	'develop/css/style/*',
+        sass:	'develop/css/style/*.scss',
+        js: 	'develop/js/*.js',
+        images: 'develop/images/**/*.{png,jpg,gif,ico}',
+        html: 	@html1@,
+        all:    'develop/**/*',
+        devCss:	'develop/css/*.css',
+        outCss: 'develop/css'
+    };
 
-gulp.watch([path.images,path.html,path.js], ['reload']);
+gulp.watch([path.images],['imgLoad']);
 gulp.watch([path.css,path.sass],['css']);
+gulp.watch([path.html,path.js], ['reload']);
+
 
 function errrHandler(e) {
 	console.log(e);
 }
 
-gulp.task('css',['clean'],() => {
+gulp.task('css',() => {
     return gulp
         .src(path.css)
         .pipe(plumber({errorHandler: errrHandler }))
@@ -42,17 +36,16 @@ gulp.task('css',['clean'],() => {
         .pipe(bs.stream());
         
 });
-gulp.task('clean', () => {
-    del.sync([path.devCss]);
-});
 gulp.task('reload', () => {
     bs.reload();
 });
-gulp.task('default',['clean','css'], function () {
+gulp.task('imgLoad', () => {
+    ScanFn.init();
+});
+gulp.task('default',['css'], function () {
     bs.init({
         server: './develop',
-        index: 'index.html',
-        browser: 'google chrome',
+        index: @html2@,
         port: '8222',
         open: 'external',
         ui: {
@@ -60,3 +53,132 @@ gulp.task('default',['clean','css'], function () {
         }
     })
 });
+
+var ScanFn = (function(){
+    var fs = require('fs'),
+        outJS = [],
+        inPath = ['develop/images'],
+        outPath = 'develop/js/img.js',
+        fileInfo = {
+            b: 'var manifest = [',
+            e: '];'
+        };
+
+    //处理某个类目下所有文件及目录
+    function forFiles(files, file_path,callback,allFilesDoneCallback) {
+        var arrlength=files.length;
+        if(!files||files.length==0){
+            allFilesDoneCallback(file_path);
+            return;
+        }
+
+        files.forEach(function (e, i) {
+            var fullFilePath = file_path + '/' + e;
+
+            fs.stat(fullFilePath, function (err, stat) {
+                var result={
+                    isDir:false,
+                    isFile:true,
+                    file:fullFilePath
+                };
+
+                if (stat.isDirectory()) {
+                    result.isDir=true;
+                    result.isFile=false;
+                }else{
+                    result.isDir=false;
+                    result.isFile=true;
+                }
+                callback(result);
+                arrlength--;
+                if(arrlength==0){
+                    allFilesDoneCallback(file_path);
+                }
+            });
+        });
+    }
+
+    //处理单个目录
+    function forDir(dirPath,watchDir,callback){
+        fs.readdir(dirPath, function (err, files) {
+            var tempArr = [],
+                subFiles= [];
+
+            files.forEach(function(file){
+                if(file.indexOf('.') < 0) {
+                    tempArr.push(file);
+                }
+            });
+
+            forFiles(tempArr,dirPath,function(result){
+                if(result.isDir){
+                    watchDir.push(result.file);
+                    forDir(result.file,watchDir,callback);
+                }else{
+                    if(result.file.indexOf('.DS_Store') < 0) {
+                        var resfile = result.file.split('/');
+                        resfile.shift();
+                        resfile = resfile.join('/');
+                        subFiles.push(resfile);
+                        outJS.push("    {src:'"+resfile+"', id:''}");
+                    }
+                }
+            },function(processedDirPath){
+                callback(processedDirPath,subFiles);
+            });
+        });
+    }
+
+    //遍历处理多个类目
+    function forDirs(dirs,doneCallback) {
+        var copiedDirs=dirs.slice(0),
+            watchDir=[],
+            allFiles=[];
+
+        copiedDirs.forEach(function(path){
+            watchDir.push(path);
+            forDir(path,watchDir,function(processedDirPath,subFiles){
+                allFiles=allFiles.concat(subFiles);
+                console.log(processedDirPath+' 处理完成',);
+                watchDir.splice(watchDir.indexOf(processedDirPath),1);
+                if(watchDir.length==0){
+                    doneCallback(allFiles);
+                }
+            });
+        });
+    }
+
+    //写入js文件
+    function wf(data){
+        fs.writeFile(outPath, data+"\n", {flag:'a'}, function(err){
+            if(err) throw err;
+            console.log("写入成功");
+        });
+    }
+
+    return {
+        init: function(){
+            outJS = [];
+            fs.stat(outPath, function(err, stat){
+                if(stat&&stat.isFile()) {
+                    fs.unlink(outPath, function (err) {
+                        if (err) {
+                            return console.error(err);
+                        }
+                        wf(fileInfo.b);
+                        forDirs(inPath,function(fileList){
+                            console.log('所有目录遍历完成,获取到文件个数为:'+fileList.length);
+                            wf(outJS.join(",\n") + "\n" + fileInfo.e);
+                        });
+                    });
+                } else {
+                    wf(fileInfo.b);
+                    forDirs(inPath,function(fileList){
+                        console.log('所有目录遍历完成,获取到文件个数为:'+fileList.length);
+                        wf(outJS.join(",\n") + "\n" + fileInfo.e);
+                    });
+                }
+            });
+        }
+    }
+})();
