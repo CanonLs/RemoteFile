729066ef65b6f12a846188d0a43f296c3e4703eb
CanonLs <467706369@qq.com>
2017-09-27 15:38:12 +0800

    devGulp

--- gulpfile.js
+++ gulpfile.js
@@ -70,173 +70,174 @@ gulp.task('default',['css'], function () {
 });
 
 var ScanFn = (function(){
-var fs = require('fs'),
-    outJS = [],
-    sinPath = ['develop/images'],
-    soutPath = 'develop/js/img.js',
-    fileInfo = {
-        b: 'var manifest = [',
-        e: '];'
-    };
-
-//处理某个类目下所有文件及目录
-function forFiles(files, file_path,callback,allFilesDoneCallback) {
-    var arrlength=files.length;
-    if(!files||files.length==0){
-        allFilesDoneCallback(file_path);
-        return;
-    }
-
-    files.forEach(function (e, i) {
-        var fullFilePath = file_path + '/' + e;
-
-        fs.stat(fullFilePath, function (err, stat) {
-            var result={
-                isDir:false,
-                isFile:true,
-                file:fullFilePath
-            };
-
-            if (stat.isDirectory()) {
-                result.isDir=true;
-                result.isFile=false;
-            }else{
-                result.isDir=false;
-                result.isFile=true;
-            }
-            callback(result);
-            arrlength--;
-            if(arrlength==0){
-                allFilesDoneCallback(file_path);
-            }
-        });
-    });
-}
-
-//处理单个目录
-function forDir(dirPath,watchDir,callback){
-    fs.readdir(dirPath, function (err, files) {
-        var tempArr = [],
-            subFiles= [];
-
-        files.forEach(function(file){
-            if(file.indexOf('.svn') < 0) {
-                tempArr.push(file);
-            }
-        });
-
-        forFiles(tempArr,dirPath,function(result){
-            if(result.isDir){
-                watchDir.push(result.file);
-                forDir(result.file,watchDir,callback);
-            }else{
-                if(result.file.indexOf('.DS_Store') < 0) {
-                    var resfile = result.file.split('/');
-                    resfile.shift();
-                    resfile = resfile.join('/');
-                    subFiles.push(resfile);
-                    outJS.push("    {src:'"+resfile+"', id:''}");
+    var fs = require('fs'),
+        outJS = [],
+        sinPath = ['develop/images'],
+        soutPath = 'develop/js/img.js',
+        fileInfo = {
+            b: 'var manifest = [',
+            e: '];'
+        };
+    
+    //处理某个类目下所有文件及目录
+    function forFiles(files, file_path,callback,allFilesDoneCallback) {
+        var arrlength=files.length;
+        if(!files||files.length==0){
+            allFilesDoneCallback(file_path);
+            return;
+        }
+    
+        files.forEach(function (e, i) {
+            var fullFilePath = file_path + '/' + e;
+    
+            fs.stat(fullFilePath, function (err, stat) {
+                var result={
+                    isDir:false,
+                    isFile:true,
+                    file:fullFilePath
+                };
+    
+                if (stat.isDirectory()) {
+                    result.isDir=true;
+                    result.isFile=false;
+                }else{
+                    result.isDir=false;
+                    result.isFile=true;
                 }
-            }
-        },function(processedDirPath){
-            callback(processedDirPath,subFiles);
-        });
-    });
-}
-
-//遍历处理多个类目
-function forDirs(dirs,doneCallback) {
-    var copiedDirs=dirs.slice(0),
-        watchDir=[],
-        allFiles=[];
-
-    copiedDirs.forEach(function(path){
-        watchDir.push(path);
-        forDir(path,watchDir,function(processedDirPath,subFiles){
-            allFiles=allFiles.concat(subFiles);
-            console.log(processedDirPath+' 处理完成');
-            watchDir.splice(watchDir.indexOf(processedDirPath),1);
-            if(watchDir.length==0){
-                doneCallback(allFiles);
-            }
-        });
-    });
-}
-
-//写入js文件
-function wf(data){
-    fs.writeFile(soutPath, data+"\n", {flag:'a'}, function(err){
-        if(err) throw err;
-        console.log("写入成功");
-    });
-}
-
-function regHtml(file) {
-    var regCss= /<link.*\s+href=(?:"css[^"]*"|'css[^']*'|"\.\.\/css[^"]*"|'\.\.\/css[^']*')[^<]*>/g;
-    var regJs = /<script.*\s+src=(?:"js[^"]*"|'js[^']*'|"\.\.\/js[^"]*"|'\.\.\/js[^']*').*?>.*?<\/script>/g;
-
-    var data = fs.readFileSync(inPath.root + '/' + file, "utf8");
-    var cssArr= data.match(regCss);
-    var jsArr = data.match(regJs);
-
-    if(cssArr!=null) {
-        cssArr.forEach(function(it,i,arr){
-            if(i!=arr.length-1) {
-                data = data.replace(it, '');
-            } else {
-                data = data.replace(it, '<link rel="stylesheet" href="css/style.min.css">');
-            }
+                callback(result);
+                arrlength--;
+                if(arrlength==0){
+                    allFilesDoneCallback(file_path);
+                }
+            });
         });
     }
-
-    if(jsArr!=null) {
-        jsArr.forEach(function(it,i,arr){
-            if(i!=arr.length-1) {
-                data = data.replace(it, '');
-            } else {
-                data = data.replace(it, '<script src="js/main.min.js"></script>');
-            }
-        });
-    }
-
-    fs.writeFileSync(inPath.root + '/' + file, data);
-}
-
-return {
-    init: function(){
-        outJS = [];
-        fs.stat(soutPath, function(err, stat){
-            if(stat&&stat.isFile()) {
-                fs.unlink(soutPath, function (err) {
-                    if (err) {
-                        return console.error(err);
+    
+    //处理单个目录
+    function forDir(dirPath,watchDir,callback){
+        fs.readdir(dirPath, function (err, files) {
+            var tempArr = [],
+                subFiles= [];
+    
+            files.forEach(function(file){
+                if(file.indexOf('.svn') < 0) {
+                    tempArr.push(file);
+                }
+            });
+    
+            forFiles(tempArr,dirPath,function(result){
+                if(result.isDir){
+                    watchDir.push(result.file);
+                    forDir(result.file,watchDir,callback);
+                }else{
+                    if(result.file.indexOf('.DS_Store') < 0) {
+                        var resfile = result.file.split('/');
+                        resfile.shift();
+                        resfile = resfile.join('/');
+                        subFiles.push(resfile);
+                        outJS.push("    {src:'"+resfile+"', id:''}");
                     }
+                }
+            },function(processedDirPath){
+                callback(processedDirPath,subFiles);
+            });
+        });
+    }
+    
+    //遍历处理多个类目
+    function forDirs(dirs,doneCallback) {
+        var copiedDirs=dirs.slice(0),
+            watchDir=[],
+            allFiles=[];
+    
+        copiedDirs.forEach(function(path){
+            watchDir.push(path);
+            forDir(path,watchDir,function(processedDirPath,subFiles){
+                allFiles=allFiles.concat(subFiles);
+                console.log(processedDirPath+' 处理完成');
+                watchDir.splice(watchDir.indexOf(processedDirPath),1);
+                if(watchDir.length==0){
+                    doneCallback(allFiles);
+                }
+            });
+        });
+    }
+    
+    //写入js文件
+    function wf(data){
+        fs.writeFile(soutPath, data+"\n", {flag:'a'}, function(err){
+            if(err) throw err;
+            console.log("写入成功");
+        });
+    }
+    
+    function regHtml(file) {
+        var regCss= /<link.*\s+href=(?:"css[^"]*"|'css[^']*'|"\.\.\/css[^"]*"|'\.\.\/css[^']*')[^<]*>/g;
+        var regJs = /<script[^>].*?src=(?:"js[^"]*"|'js[^']*'|"\.\.\/js[^"]*"|'\.\.\/js[^']*').*?>.*?<\/script>/g;;
+    
+        var data = fs.readFileSync(outPath.html + '/' + file, "utf8");
+        var cssArr= data.match(regCss);
+        var jsArr = data.match(regJs);
+    
+        if(cssArr!=null) {
+            cssArr.forEach(function(it,i,arr){
+                if(i!=arr.length-1) {
+                    data = data.replace(it, '');
+                } else {
+                    data = data.replace(it, '<link rel="stylesheet" href="css/style.min.css">');
+                }
+            });
+        }
+    
+        if(jsArr!=null) {
+            jsArr.forEach(function(it,i,arr){
+                if(i!=arr.length-1) {
+                    data = data.replace(it, '');
+                } else {
+                    data = data.replace(it, '<script src="js/main.min.js"></script>');
+                }
+            });
+        }
+    
+        fs.writeFileSync(outPath.html + '/' + file, data);
+    }
+    
+    return {
+        init: function(){
+            outJS = [];
+            fs.stat(soutPath, function(err, stat){
+                if(stat&&stat.isFile()) {
+                    fs.unlink(soutPath, function (err) {
+                        if (err) {
+                            return console.error(err);
+                        }
+                        wf(fileInfo.b);
+                        forDirs(sinPath,function(fileList){
+                            console.log('所有目录遍历完成,获取到文件个数为:'+fileList.length);
+                            wf(outJS.join(",\n") + "\n" + fileInfo.e);
+                        });
+                    });
+                } else {
                     wf(fileInfo.b);
                     forDirs(sinPath,function(fileList){
                         console.log('所有目录遍历完成,获取到文件个数为:'+fileList.length);
                         wf(outJS.join(",\n") + "\n" + fileInfo.e);
                     });
-                });
-            } else {
-                wf(fileInfo.b);
-                forDirs(sinPath,function(fileList){
-                    console.log('所有目录遍历完成,获取到文件个数为:'+fileList.length);
-                    wf(outJS.join(",\n") + "\n" + fileInfo.e);
-                });
-            }
-        });
-    },
-
-    reg: function(fn){
-        var files = fs.readdirSync(inPath.root);
-        files.forEach(function(file){
-            if(file.indexOf('.html') > 0) {
-                regHtml(file);
-            }
-        });
+                }
+            });
+        },
+    
+        reg: function(fn){
+            var files = fs.readdirSync(outPath.html);
+            files.forEach(function(file){
+                if(file.indexOf('.html') > 0) {
+                    regHtml(file);
+                }
+            });
+        }
     }
-}
 })();
+    
 
 // ---------------上线打包压缩--------------------
 gulp.task('js', () => {
